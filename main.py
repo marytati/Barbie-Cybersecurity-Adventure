153da73cc91d04ecc8d06249c277c4ba57716fc6
marytati <104618683+marytati@users.noreply.github.com>
2024-10-07 12:01:16 +0100

    Create main.py
    Task: Write a simple script where Barbie asks for a password to enter her Dream House, and it responds with a welcome message.

---
+++ main.py
@@ -0,0 +1,26 @@
+#Task: Write a simple script where Barbie asks for a password to enter her Dream House, and it responds with a welcome message.
+
+import os 
+from github import github
+#password
+dreamhouse_password = "pink123"
+# Ask the user for the password
+print("👑 Barbie: Welcome to the Dream House! Please enter the secret password to enter.")
+
+#asking for the password 
+user_password =  input("Enter the password")
+
+if user_password == dreamhouse_password : 
+      print("🎉 Barbie: Yay! Welcome to the Dream House! ✨🏰")
+else:
+  print("🚫 Barbie: Oops! That's not the right password. Try again next time.")
+  
+  Gifts = " Yes"
+  Gift_question = input(" Did you bring a gift?")
+                        if gifts_password == gifts: 
+                        print("Thank you! Come in")
+                        else:
+                        print(" Sorry Barbie isn't accepting visitors")
+
+
+
