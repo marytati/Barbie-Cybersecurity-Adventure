3a9a199984d339fbf9b39fab4ad41f2c4b1ae9bf
marytati <104618683+marytati@users.noreply.github.com>
2024-10-08 11:28:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from github import github
 #password
 dreamhouse_password = "pink123"
 # Ask the user for the password
+def ask_for_password():
 print("👑 Barbie: Welcome to the Dream House! Please enter the secret password to enter.")
 
 #asking for the password 
 user_password =  input("Enter the password")
+return user_password
 
 if user_password == dreamhouse_password : 
       print("🎉 Barbie: Yay! Welcome to the Dream House! ✨🏰")
@@ -22,5 +24,16 @@ else:
                         else:
                         print(" Sorry Barbie isn't accepting visitors")
 
+# Function to check if the password is correct
+def check_password(attempts):
+    # Allow the user to try up to a certain number of attempts
+while attempts > 2:
+       # Get the password from the user
+      user_password = ask_for_password
+def(welcome_guests):
+    # List of guest names
+guest = ["Ken", "Skipper", "Chelsea"]
+    print("👑 Barbie: Let's welcome some guests to the Dream House!")
+# Run the program
 
-
+main()
