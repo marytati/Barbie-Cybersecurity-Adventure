c26376f659eac817d1bacf1182fe2ddf0b56d711
marytati <104618683+marytati@users.noreply.github.com>
2024-10-08 12:57:23 +0100

    Create Generating Random-Secure-Password.py

---
+++ Generating Random-Secure-Password.py
@@ -0,0 +1,8 @@
+import secrets
+ # For generating cryptographically secure random numbers
+import strings 
+# For accessing character sets like letters, digits, punctuation
+# Function to generate a secure password
+def generate_password(length=12):
+    # Define character sets to use in the password
+alphabet = (string.digits = string.punctuations) 
