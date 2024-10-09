5a541cc566da49742827d3cb462559917cb8f80b
marytati <104618683+marytati@users.noreply.github.com>
2024-10-09 13:05:29 +0100

    Update pwned.py

--- pwned.py
+++ pwned.py
@@ -8,4 +8,7 @@ return sha1_hash
 def check_password_pwned(password) :
   # This part calls another function (not shown) that hashes the password using the SHA-1 method (like you learned before).
   hash_password(password) = sha1_hash #Stores the resulting hash from the password.
-  
+      # Step 2: Send the first 5 characters of the SHA-1 hash to HIBP API
+first_5_chars = sha1_hash [:5]
+rest_of_hash = sha1_hash [5:]
+
