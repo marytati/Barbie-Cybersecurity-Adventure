0574b9ef1c57a31464f38e473266b88b64d941ee
marytati <104618683+marytati@users.noreply.github.com>
2024-10-09 12:59:18 +0100

    Create pwned.py

---
+++ pwned.py
@@ -0,0 +1,11 @@
+import requests # For making API requests
+import hashlib # For hashing the password
+def hash_password(password):
+      # Hash the password using SHA-1
+sha1_hash = hashlibs.sha1(password.encode('utf-8')).hexidigest().upper().
+return sha1_hash
+#This is a function that checks if a password has been leaked (pwned) before.
+def check_password_pwned(password) :
+  # This part calls another function (not shown) that hashes the password using the SHA-1 method (like you learned before).
+  hash_password(password) = sha1_hash #Stores the resulting hash from the password.
+  
